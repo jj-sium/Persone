fc99134ef3260e29671b6cce29c8c27d0bdaea94
Johan Valentino Ghinelli <116793076+jj-sium@users.noreply.github.com>
2023-03-17 09:01:10 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ghinelli.johan._4h.persone.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static ghinelli.johan._4h.persone.Models.Persona;
+
+namespace ghinelli.johan._4h.persone
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ATA a = new ATA();
+            Debug.WriteLine(a);
+        }
+        private void dgbDati_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            
+        }
+
+        private void btnApriFile_Click(object sender, RoutedEventArgs e)
+        {
+            ATA a = new ATA()
+            Debug.WriteLine("Sono io:" + a);
+        }
+    }
+}
---
+++ Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ghinelli.johan._4h.persone.Models
+{
+    public enum TipoPersona { NonDefinitp, ATA, Alunno, Docente };
+    public class Persona
+    {
+        public TipoPersona Tipo{ get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cognome { get; set; }
+
+        public string Email { get; set; }
+
+        public class ATA : Persona
+        {
+            public ATA()
+            {
+                Tipo = TipoPersona.ATA;
+            }
+        }
+    }
+}
